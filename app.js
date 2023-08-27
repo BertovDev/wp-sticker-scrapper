4f58194442e11241514ee71af361641497f8f929
BertovDev <bertobautista18@gmail.com>
2023-08-27 16:33:38 -0300

    Clean up code and fix message search

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const { Console } = require("console");
 const fs = require("fs");
 const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
-const groupToScrapp = "group name";
+const GROUP_TO_SCRAPP = "GROUP NAME";
+const LIMIT_MESSAGES_TO_SCRAPP = 1000;
 
 const client = new Client({
   authStrategy: new LocalAuth(),
@@ -17,37 +19,47 @@ const getChats = async () => {
   return chats.filter((chat) => chat.isGroup);
 };
 
+const getMessagesWithMedia = (messages) => {
+  return messages.filter((message) => message.hasMedia);
+};
+
+const saveMedia = (el, media) => {
+  const mediaName = "media " + el.id.id.slice(0, 5);
+  fs.writeFile(
+    "./upload/" + mediaName + ".png", // remove .png to save them in webp format
+    media.data,
+    "base64",
+    function (data) {
+      console.log("Downloading " + mediaName);
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+};
+
+const downloadAndSaveStickers = async (messages) => {
+  getMessagesWithMedia(messages).forEach(async (msg) => {
+    const media = await msg.downloadMedia();
+    //only save stickers
+    if (media.mimetype === "image/webp") {
+      saveMedia(msg, media);
+    }
+  });
+};
+
 client.on("ready", async () => {
   console.log("client is ready");
   const groupChats = await getChats();
-  const group = groupChats.filter((chat) => chat.name === groupToScrapp);
-  console.log(JSON.stringify(group));
-  const messages = await group[0].fetchMessages(200, true);
-  messages.forEach(async (el, i) => {
-    // console.log(el.mediaKey);
-    // console.log(el.type);
-    if (el.hasMedia) {
-      const media = await el.downloadMedia();
-      //   console.log(media.mediak);
-      if (media.mimetype === "image/webp") {
-        fs.writeFile(
-          "./upload/" + "media " + i,
-          media.data,
-          "base64",
-          function (data) {
-            const filename =
-              media.filename !== undefined ? media.filename : "Media " + i;
-            console.log("Downloading" + filename);
-          },
-          function (err) {
-            if (err) {
-              console.log(err);
-            }
-          }
-        );
-      }
-    }
+  const group = groupChats.filter((chat) => chat.name === GROUP_TO_SCRAPP);
+
+  const messages = await group[0]?.fetchMessages({
+    limit: LIMIT_MESSAGES_TO_SCRAPP,
   });
+
+  await downloadAndSaveStickers(messages);
 });
 
 client.initialize();
